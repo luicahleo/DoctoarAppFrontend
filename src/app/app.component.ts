64ad81dbc0df5d44ba934136a1ffbe60febddc5e
luis <luisrolandocahuanaleon@gmail.com>
2024-03-30 11:23:49 +0100

    material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'DoctorAppFrontend';
+
+  usuarios: any;
+
+  constructor(private http: HttpClient) {}
+
+
+  ngOnInit(): void {
+
+    // this.http.get('http://localhost:5238/api/Usuario').subscribe((response) => {
+    //   this.usuarios = response;
+    // });
+
+    this.http.get('http://localhost:5238/api/Usuario').subscribe({
+      next: (response) => {
+        this.usuarios = response;
+      },
+      error: (error) => {
+        console.error('There was an error!', error);
+      },
+      complete: () => {
+        console.log('Completed');
+      },
+    });
+
+
+  }
 }
